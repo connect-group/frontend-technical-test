30d08b8d32b4909ab9a95fed9ce9e524c1419806
bretthadley <brett.hadley@connect-group.com>
2016-09-08 10:21:18 +0100

    init

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const sass = require('gulp-sass');
+const mocha = require('gulp-mocha');
+const server = require('gulp-develop-server');
+
+gulp.task('js', function () {
+   return browserify({entries: './src/app.js', extensions: ['.js'], debug: true})
+        .transform('babelify', {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('js:watch', function () {
+    gulp.watch('./src/**/*.js', ['js']);
+});
+ 
+gulp.task('sass', function () {
+    return gulp.src('./src/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
+});
+ 
+gulp.task('sass:watch', function () {
+    gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('test', () => {
+    return gulp.src('./test/*.spec.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('server', function () {
+    server.listen( { path: './server.js' } );
+});
+
+gulp.task('server:watch', function () {
+    gulp.watch( [ './server.js' ], server.restart );
+});
+
+gulp.task('default', function () {
+  gulp.start('sass', 'sass:watch', 'js', 'js:watch', 'server', 'server:watch');
+});
---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+/**
+* This is an example request. Create your own using best practises for
+* handling asynchronous data fetching
+**/
+
+export const getData = (cb) => {
+    const vehicles = new XMLHttpRequest();
+    vehicles.open('GET', 'http://localhost:9988/api/vehicle');
+
+    vehicles.onreadystatechange = function() {
+        if(vehicles.readyState === 4) {
+ 		    if(vehicles.status === 200) {
+ 			    cb(vehicles.responseText);
+		    }
+		}
+	}
+
+	vehicles.send();
+}
