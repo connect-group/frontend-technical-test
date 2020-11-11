beb98b97781745631ce9f69b50993e777f7560a4
johnloydall <63065517+johnloydall@users.noreply.github.com>
2020-11-11 16:00:41 +0000

    Rest api.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,42 +4,16 @@
 **/
 
 export const getData = (cb) => {
-	const vehicles = new XMLHttpRequest();
-	vehicles.open('GET', 'http://localhost:9988/api/vehicle');
+    const vehicles = new XMLHttpRequest();
+    vehicles.open('GET', 'http://localhost:9988/api/vehicle');
 
-	vehicles.onreadystatechange = function () {
-		if (vehicles.readyState === 4) {
-			if (vehicles.status === 200) {
-				cb(JSON.parse(vehicles.response));
-			} else {
-				console.log(`Something wrong has happened. Status: ${vehicles.status}`);
-			}
+    vehicles.onreadystatechange = function() {
+        if(vehicles.readyState === 4) {
+ 		    if(vehicles.status === 200) {
+ 			    cb(vehicles.responseText);
+		    }
 		}
 	};
 
 	vehicles.send();
 };
-
-export const getVehicleDetails = (id, callback) => {
-	fetch(`http://localhost:9988/api/vehicle/${id}`, {
-		method: "GET",
-		headers: { "Content-Type": "application/json" },
-	})
-		.then(response => {
-			if (response.ok) {
-				return response.json();
-			} else {
-				console.log(`Something wrong has happened. Status: ${response.status} ${response.statusText}`);
-			}
-		})
-		.then(data => {
-			if (data) {
-				callback(data);
-			} else {
-				console.log(`Something wrong has happened. Error: couldn't get data.`);
-			}
-		})
-		.catch(e => {
-			console.log(`Something wrong has happened. Error: ${e}`);
-		})
-}
