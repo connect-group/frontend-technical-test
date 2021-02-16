d7509140ac05360f1ef2b411cb063ac0b6b732d5
Muhammad Khan <muhammadshoaibkhan706@gmail.com>
2021-02-16 18:19:27 +0500

    updated component

--- src/components/VehicleList.js
+++ src/components/VehicleList.js
@@ -1,33 +1,68 @@
-import React, { Component } from 'react';
-import { getData } from '../api';
+import React, { Component } from "react";
+import { getData, getVehicleAdditionalInformation } from "../api";
+import Vehicle from "./Vehicle";
+import { Container, Grid } from "@material-ui/core";
 
-export default
-class VehicleList extends Component {
+const styles = {
+  root: {
+    padding: 8,
+    backgroundColor: "white",
+  },
+};
 
-	constructor(props) {
-		super(props);
+export default class VehicleList extends Component {
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			data: null
-		}
-	}
+    this.state = {
+      data: null,
+      error: null,
+    };
+  }
 
-	componentDidMount() {
-		getData((data) => {
-			this.setState({
-				data
-			})
-		});
-	}
+  async componentDidMount() {
+    document.body.style.margin = 0;
+    try {
+      const response = await getData();
+      this.setState({
+        data: response.vehicles,
+      });
+    } catch (e) {
+      console.log(e);
+      this.setState({
+        error:
+          "We are facing internal server error. We apologize for this inconvenience",
+      });
+    }
+  }
 
-	render() {
-		if(this.state.data) {
-			console.log(this.state.data);
-		    return (
-			    <h1>Hello World</h1>
-		    )
-	    }
+  async getVehicleInformation(vehicleId) {
+    return await getVehicleAdditionalInformation(vehicleId);
+  }
 
-		return (<h1>Loading...</h1>);
-	}
+  render() {
+    if (!this.state.data && !this.state.error) {
+      return <h1>Loading....</h1>;
+    }
+
+    if (!this.state.data && this.state.error) {
+      return <h1>{this.state.error}</h1>;
+    }
+	
+    return (
+      <Container maxWidth={false} style={styles.root}>
+        <Grid container spacing={1}>
+          {this.state.data.map((item, index) => (
+            <Grid key={index} item lg={6} md={4} xs={12}>
+              <Vehicle
+                key={index}
+                vehicle={item}
+                getVehicleInformation={this.getVehicleInformation}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    );
+  }
 }
