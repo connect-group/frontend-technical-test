b965a2350cd2080781406a7fd78a2ba33c8a140e
Pablo de Yzaguirre <p.deyzaguirre13@gmail.com>
2022-08-08 13:10:34 +0200

    Test completion

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -4,6 +4,20 @@
  * @param {string} apiUrl
  * @return {Promise<Object>}
  */
-export async function request(apiUrl) {
-  return apiUrl;
+export async function request(vehicle) {
+  let finalData = {};
+  try {
+    const response = await fetch(vehicle.apiUrl);
+    finalData = {
+      ...vehicle,
+      ...(await response.json()),
+    };
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+  return finalData;
+}
+
+export function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import VehicleList from './components/VehicleList';
+import { ModalContextProvider } from './context/Modal';
 import './global-styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <VehicleList />
+   <ModalContextProvider>
+      <VehicleList />
+    </ModalContextProvider>
   </React.StrictMode>,
   document.querySelector('.root')
 );
