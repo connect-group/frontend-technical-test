42913dd6f9f24a2013a31c051b089ff972e25db2
John Mailey# <john.mailey@gmail.com>
2022-12-27 22:17:42 +0000

    get data

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import { request } from './helpers';
+import { noEmptyValues, request } from './helpers';
 
+const acceptableData = ({ status, value }) => status === 'fulfilled' && noEmptyValues(value);
 /**
  * Pull vehicles information
  *
@@ -8,5 +9,18 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  const vehicles = await request('/api/vehicles.json ');
+  const apiUrls = vehicles.map(({ apiUrl }) => apiUrl);
+  const vehicleDetails = await Promise.allSettled(apiUrls.map((url) => request(url))).catch((error) => console.log(error));
+  const filteredVehicles = vehicleDetails.filter(acceptableData).map(({ value }) => (value));
+  const vehicleSummaryPayload = filteredVehicles.map((data) => {
+    const vehicle = { ...data };
+    const match = vehicles.find((obj) => data.id === obj.id);
+    if (match) {
+      vehicle.media = match.media;
+      vehicle.modelYear = match.modelYear;
+    }
+    return vehicle;
+  });
+  return vehicleSummaryPayload;
 }
