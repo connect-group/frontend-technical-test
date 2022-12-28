14f4765414da730e2265c62486b46788ed1bc0a7
John Mailey# <john.mailey@gmail.com>
2022-12-28 20:00:14 +0000

    add modal to vehicleList

--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import useData from './useData';
 import './style.scss';
+import Vehicle from '../Vehicle';
+import Modal from '../Modal';
+import useStore from '../Context/useStore';
 
 export default function VehicleList() {
   const [loading, error, vehicles] = useData();
+  const { state: { modal } } = useStore();
 
   if (loading) {
     return <div data-testid="loading">Loading</div>;
@@ -14,30 +18,13 @@ export default function VehicleList() {
   }
 
   return (
-    <div data-testid="results">
-      <div className="container">
-        {vehicles.map((data) => <Vehicle data={data} key={data.id} />)}
+    <>
+      <div data-testid="results">
+        <div className="container">
+          {vehicles.map((data) => <Vehicle data={data} key={data.id} />)}
+        </div>
       </div>
-    </div>
-  );
-}
-
-function Vehicle({ data }) {
-  const {
-    media, id, price
-  } = data;
-  return (
-    <div className="card">
-      <div className="card__header">
-        <img src={media[0].url} alt={id} />
-      </div>
-      <div className="card__body">
-        <p className="card__name">{id}</p>
-        <p className="card__price">
-          {`From ${price}`}
-        </p>
-        <p className="card__description">The pinnacle of refined capability</p>
-      </div>
-    </div>
+      {modal.isOpen && <Modal />}
+    </>
   );
 }
