734a99a8dc5b215496642fdd154303b9342390b8
John Mailey# <john.mailey@gmail.com>
2022-12-28 19:59:58 +0000

    wrap app in context provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Provider from './components/Context';
 import VehicleList from './components/VehicleList';
 import './global-styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <VehicleList />
+    <Provider>
+      <VehicleList />
+    </Provider>
   </React.StrictMode>,
   document.querySelector('.root')
 );
