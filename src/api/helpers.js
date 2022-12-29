9795b23ab28d6cfdb371430997ec7ea939095a3d
John Mailey# <john.mailey@gmail.com>
2022-12-29 15:52:23 +0000

    move function to utils

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -8,9 +8,3 @@ export async function request(apiUrl) {
   const response = await fetch(apiUrl);
   return response.json();
 }
-
-export function noEmptyValues(values) {
-  const valuesToCheck = ['price'];
-  const notEmpty = (value) => values[value] !== '' && values[value] !== undefined && values[value] !== null;
-  return valuesToCheck.some(notEmpty);
-}
