294f3c6c0da6b5b1c0a3fbb8997ea17fd4f5c926
John Mailey# <john.mailey@gmail.com>
2023-01-02 17:33:34 +0000

    tidy

--- src/api/index.js
+++ src/api/index.js
@@ -10,9 +10,9 @@ const acceptableData = ({ status, value }) => status === 'fulfilled' && noEmptyV
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  const vehicles = await request('/api/vehicles.json ');
+  const vehicles = await request('/api/vehicles.json');
   const apiUrls = vehicles.map(({ apiUrl }) => apiUrl);
-  const vehicleDetailsResponse = await Promise.allSettled(apiUrls.map((url) => request(url))).catch((error) => console.log(error));
+  const vehicleDetailsResponse = await Promise.allSettled(apiUrls.map((url) => request(url)));
   const vehicleDetails = vehicleDetailsResponse.filter(acceptableData).map(({ value }) => (value));
   const vehicleSummaryPayload = mergeVehicleData(vehicles, vehicleDetails);
   return vehicleSummaryPayload;
