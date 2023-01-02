54dc16cd2b2f8178b07ba3b1acac12483e412e6f
John Mailey# <john.mailey@gmail.com>
2023-01-02 17:31:50 +0000

    prefetch image for modal open

--- src/components/Vehicle/index.js
+++ src/components/Vehicle/index.js
@@ -8,20 +8,23 @@ export default function Vehicle({ data }) {
   } = data;
   const { dispatch } = useStore();
   return (
-    <button type="button" onClick={() => dispatch({ type: 'TOGGLEMODAL', payload: { isOpen: true, data } })}>
-      <div className="card">
-        <picture className="card__image">
-          <source media="(min-width:768px)" srcSet={media[0].url} />
-          <img src={media[1].url} alt={id} />
-        </picture>
-        <div className="card__body">
-          <p className="card__name">{id}</p>
-          <p className="card__price">
-            {`From ${price}`}
-          </p>
-          <p className="card__description">The pinnacle of refined capability</p>
+    <>
+      <link rel="prefetch" href={media[0].url} as="image" />
+      <button type="button" onClick={() => dispatch({ type: 'TOGGLEMODAL', payload: { isOpen: true, data } })}>
+        <div className="card">
+          <picture className="card__image">
+            <source media="(min-width:768px)" srcSet={media[0].url} />
+            <img src={media[1].url} alt={id} />
+          </picture>
+          <div className="card__body">
+            <p className="card__name">{id}</p>
+            <p className="card__price">
+              {`From ${price}`}
+            </p>
+            <p className="card__description">The pinnacle of refined capability</p>
+          </div>
         </div>
-      </div>
-    </button>
+      </button>
+    </>
   );
 }
