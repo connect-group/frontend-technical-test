7483fe9b84ecec9ced43e88962bc835a8d76b424
Ed Henderson <ed@neep.scot>
2023-11-09 16:59:14 +0000

    Fix path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import VehicleList from './components/VehicleList';
+import VehicleList from './components/VehicleList/VehicleList';
 
 ReactDOM.render(
   <React.StrictMode>
