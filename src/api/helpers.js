11cd1b5390e2d6330b7e15f815c1981cc850a282
Ed Henderson <ed@neep.scot>
2023-11-09 12:58:42 +0000

    Fixing (fleshing out) `request` so that it works as intended

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -5,5 +5,17 @@
  * @return {Promise<Object>}
  */
 export async function request(apiUrl) {
-  return apiUrl;
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching data: ', error);
+    throw error;
+  }
 }
