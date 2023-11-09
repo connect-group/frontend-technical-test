0b12540477f3940f4aca9021cf1e2f731c8f80c9
Ed Henderson <ed@neep.scot>
2023-11-09 16:56:25 +0000

    Rename of VehicleList files

---
+++ src/components/VehicleList/VehicleList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import useData from './useData';
+
+export default function VehicleList() {
+  // eslint-disable-next-line no-unused-vars
+  const { loading, error, vehicles } = useData();
+
+  if (loading) {
+    return <div data-testid="loading">Loading</div>;
+  }
+
+  if (error) {
+    return <div data-testid="error">{ error }</div>;
+  }
+
+  return (
+    <section data-testid="results">
+      <ul className="vehicle-list">
+        {vehicles.map((vehicle) => <Vehicle vehicle={vehicle} />)}
+      </ul>
+    </section>
+  );
+}
