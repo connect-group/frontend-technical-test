e28da8ac8a1f7fb0e647fa2dc4d1f45d9bab0218
Ed Henderson <ed@neep.scot>
2023-11-09 16:55:55 +0000

    Remove unused global styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import VehicleList from './components/VehicleList';
-import './global-styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
