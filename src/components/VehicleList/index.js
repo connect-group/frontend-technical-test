336f793abbb20750c547c1f77b60303aad0c5c59
robertjezyk <rjezyk@vp.pl>
2024-01-22 18:06:19 +0000

    Adding Tuffy font. responsive vehicle tiles

--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -4,7 +4,7 @@ import "./style.scss";
 
 export default function VehicleList() {
   const [loading, error, vehicles] = useData();
-  console.log(vehicles);
+  // console.log(vehicles);
 
   if (loading) {
     return <div data-testid="loading">Loading</div>;
@@ -16,33 +16,38 @@ export default function VehicleList() {
 
   return (
     <section data-testid="results" className="vehicles">
-      {vehicles.map((vehicle) => {
-        const [{ url: imageLargeUrl }, { url: imageSmallUrl, name: altText }] =
-          vehicle.media;
+      {vehicles
+        // .concat(vehicles)
+        // .concat(vehicles) // To test for larger amount of data
+        .map((vehicle) => {
+          const [
+            { url: imageLargeUrl },
+            { url: imageSmallUrl, name: altText },
+          ] = vehicle.media;
 
-        return (
-          <article className="vehicle-card" key={vehicle.id}>
-            <div className="vehicle-card__image-container">
-              <picture>
-                <source media="(max-width: 600px)" srcSet={imageSmallUrl} />
-                <source media="(min-width: 601px)" srcSet={imageLargeUrl} />
-                <img
-                  className="vehicle-card__image"
-                  src={imageSmallUrl}
-                  alt={altText}
-                />
-              </picture>
-            </div>
-            <div className="vehicle-card__info">
-              <h2 className="vehicle-car__name">{vehicle.id}</h2>
-              <h3 className="vehicle-car__price">{vehicle.details.price}</h3>
-              <p className="vehicle-car__description">
-                {vehicle.details.description}
-              </p>
-            </div>
-          </article>
-        );
-      })}
+          return (
+            <article className="vehicle-card" key={vehicle.id}>
+              <div className="vehicle-card__image-container">
+                <picture>
+                  <source media="(max-width: 600px)" srcSet={imageSmallUrl} />
+                  <source media="(min-width: 601px)" srcSet={imageLargeUrl} />
+                  <img
+                    className="vehicle-card__image"
+                    src={imageSmallUrl}
+                    alt={altText}
+                  />
+                </picture>
+              </div>
+              <div className="vehicle-card__info">
+                <h2 className="vehicle-card__name">{vehicle.id}</h2>
+                <h3 className="vehicle-card__price">{`From ${vehicle.details.price}`}</h3>
+                <p className="vehicle-card__description">
+                  {vehicle.details.description}
+                </p>
+              </div>
+            </article>
+          );
+        })}
     </section>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,51 +4,55 @@ const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: path.resolve(__dirname, "src", "index.js"),
+  entry: {
+    index: path.resolve(__dirname, "src", "index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    static: {
+      directory: path.join(__dirname, "public"),
     },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
-    },
-    devServer: {
-        hot: true,
-        open: true,
-        static: {
-            directory: path.join(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "public", "api"),
-                    to: "api",
-                },
-                {
-                    from: path.resolve(__dirname, "public", "images"),
-                    to: "images",
-                },
-            ],
-        }),
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public", "api"),
+          to: "api",
+        },
+        {
+          from: path.resolve(__dirname, "public", "images"),
+          to: "images",
+        },
+      ],
+    }),
+  ],
 };
